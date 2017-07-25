f0b19646f8160fb479440d5640cb51508f22dda2
igormusich <igor.music@rbc.com>
2017-07-25 23:07:40 +0200

    Added more unit tests

--- src/test/java/com/rbc/iam/policy/PolicyServiceTest.java
+++ src/test/java/com/rbc/iam/policy/PolicyServiceTest.java
@@ -1,18 +1,13 @@
 package com.rbc.iam.policy;
 
 import com.rbc.iam.policy.model.*;
-import com.rbc.iam.policy.repository.PolicyRepository;
 import com.rbc.iam.policy.service.PolicyService;
-import com.rbc.iam.policy.service.PolicyServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
@@ -71,7 +66,7 @@ public class PolicyServiceTest {
                 Arrays.asList("groups:admins"),
                 Arrays.asList("resources:profile:<.*>"),
                 Arrays.asList("approve"),
-                 "approve");
+                 "allow");
 
         testPolicy.addCondition("isBankUser", new StringEqualCondition("true"));
         testPolicy.addCondition("isWorkingHours", new StringEqualCondition("true"));
@@ -91,9 +86,78 @@ public class PolicyServiceTest {
         };
 
         assertThat(policyService.isAllowed(request), equalTo(true));
-
-
     }
 
+    @Test
+    public void george_can_create_articles_returns_false() {
+        Request request = new Request() {
+            {
+                subject ="users:george";
+                resource="resources:articles:";
+                action="create";
+            }
+        };
+
+        assertThat(policyService.isAllowed(request), equalTo(false));
+    }
+
+    @Test
+    public void admins_can_delete_printers_returns_true() {
+        Request request = new Request() {
+            {
+                subject ="groups:admins";
+                resource="resources:printer";
+                action="delete";
+            }
+        };
+
+        assertThat(policyService.isAllowed(request), equalTo(true));
+    }
+
+    @Test
+    public void any_user_can_update_his_address() {
+        Request request = new Request() {
+            {
+                subject ="users:anon";
+                resource="resources:profile:address";
+                action="update";
+            }
+        };
+
+        request.getContext().setProperty("resourceOwner","users:anon");
+
+        assertThat(policyService.isAllowed(request), equalTo(true));
+    }
+
+    @Test
+    public void any_user_auhorizing_changes_to_profile_returns_false() {
+        Request request = new Request() {
+            {
+                subject ="users:anon";
+                resource="resources:profile:";
+                action="approve";
+            }
+        };
+
+        request.getContext().setProperty("resourceOwner","users:anon");
+
+        assertThat(policyService.isAllowed(request), equalTo(false));
+    }
+
+    @Test
+    public void admin_user_authorizing_changes_to_profile_returns_true() {
+        Request request = new Request() {
+            {
+                subject ="groups:admins";
+                resource="resources:profile:";
+                action="approve";
+            }
+        };
+
+        request.getContext().setProperty("isBankUser","true");
+        request.getContext().setProperty("isWorkingHours","true");
+
+        assertThat(policyService.isAllowed(request), equalTo(true));
+    }
 }
 
